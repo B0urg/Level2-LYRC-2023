40425b053a63c942e560fe3e11c242982ea84cc5
Felix Gasiaux <felixgasiaux@gmail.com>
2023-03-16 16:05:30 +0100

    changed motor ports

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,9 +1,9 @@
 using namespace vex;
 
 extern brain Brain;
-vex::motor      motor_right(vex::PORT15, vex::gearSetting::ratio18_1, true);
-vex::motor      motor_left(vex::PORT16, vex::gearSetting::ratio18_1, false);
-vex::controller controller1(vex::controllerType::primary);
+extern vex::motor      motor_right;
+extern vex::motor      motor_left;
+extern vex::controller controller1;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
