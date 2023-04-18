ecc77ff2f9782b963d3301a2145f21afba6dc765
b0urg <kim@bourg.dev>
2023-04-18 16:55:34 +0200

    added some shooting related motors & sensors

--- include/robot-config.h
+++ include/robot-config.h
@@ -5,8 +5,11 @@ extern vex::motor      motor_right;
 extern vex::motor      motor_left;
 extern vex::controller controller1;
 extern vex::motor      catapult_motor;
-extern vex::bumper     BumperB;
-
+extern vex::bumper     BumperA;
+extern vex::limit      LimitB;
+extern vex::motor      expandLeft;
+extern vex::motor      expandRight;
+extern vex::motor      intakeMotor;
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  *
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -4,11 +4,15 @@ using namespace vex;
 
 // A global instance of brain used for printing to the V5 brain screen
 brain Brain;
-vex::motor      motor_right(vex::PORT6, vex::gearSetting::ratio18_1, false);
-vex::motor      motor_left(vex::PORT7, vex::gearSetting::ratio18_1, true);
+vex::motor      motor_right(vex::PORT1, vex::gearSetting::ratio18_1, false);
+vex::motor      motor_left(vex::PORT2, vex::gearSetting::ratio18_1, true);
 vex::controller controller1(vex::controllerType::primary);
-vex::motor      catapult_motor(vex::PORT8, vex::gearSetting::ratio18_1, false);
-vex::bumper     BumperB(Brain.ThreeWirePort.B);
+vex::motor      catapult_motor(vex::PORT3, vex::gearSetting::ratio18_1, false);
+vex::bumper     BumperA(Brain.ThreeWirePort.A);
+vex::limit      LimitB(Brain.ThreeWirePort.B);
+vex::motor      expandLeft(vex::PORT4, vex::gearSetting::ratio18_1, false);
+vex::motor      expandRight(vex::PORT5, vex::gearSetting::ratio18_1, true);
+vex::motor      intakeMotor(vex::PORT6, vex::gearSetting::ratio18_1, false);
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  *
